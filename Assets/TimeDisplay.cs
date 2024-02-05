33b7d13469d88d37aaa16d410479d94b47e66db2
jacobmarc01 <91617543+jacobmarc01@users.noreply.github.com>
2024-02-05 17:10:30 -0600

    Update TimeDisplay.cs

--- Assets/TimeDisplay.cs
+++ Assets/TimeDisplay.cs
@@ -5,12 +5,5 @@ using TMPro;
 
 public class TimeDisplay : MonoBehaviour
 {
-    public int stars = 0; // The variable to display
-  public TMP_Text starText; // The TextMeshPro object to display
-
-  // Update is called once per frame
-  void Update()
-  {
-    starText.SetText("Stars: " + stars);
-  }
+  
 }
