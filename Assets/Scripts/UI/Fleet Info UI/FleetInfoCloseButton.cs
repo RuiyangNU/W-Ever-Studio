e01de7da9a46fd974a524933d6f2414e42a03cb7
James Zieper <jameszieper2025@u.northwestern.edu>
2024-02-06 20:15:45 -0600

    Fleet UI and functions

---
+++ Assets/Scripts/UI/Fleet Info UI/FleetInfoCloseButton.cs
@@ -0,0 +1,24 @@
+using UnityEngine.UI;
+
+public class FleetInfoCloseButton : PopupUIElement
+{
+    private Image _image;
+    private Button _button;
+
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        _button = GetComponent<Button>();
+    }
+
+    override public void OnUIClose()
+    {
+        _image.enabled = false;
+        _button.enabled = false;
+    }
+    override public void OnUIOpen()
+    {
+        _image.enabled = true;
+        _button.enabled = true;
+    }
+}
---
+++ Assets/Scripts/UI/Fleet Info UI/FleetInfoFleetButton.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FleetInfoFleetButton : PopupUIElement
+{
+    public bool showOnOpen = true;
+
+    private Image _image;
+    private Button _button;
+
+    private TextMeshProUGUI buttonText;
+
+    void Awake()
+    {
+        _image = GetComponent<Image>();
+        _button = GetComponent<Button>();
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    override public void OnUIClose()
+    {
+        _image.enabled = false;
+        _button.enabled = false;
+        buttonText.enabled = false;
+    }
+    override public void OnUIOpen()
+    {
+        if (showOnOpen)
+        {
+            _image.enabled = true;
+            _button.enabled = true;
+            buttonText.enabled = true;
+        }
+    }
+}
