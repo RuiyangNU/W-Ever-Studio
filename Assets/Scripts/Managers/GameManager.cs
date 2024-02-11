989c2915236da6cea795d5c269af8459d7f47575
jacobmarc01 <91617543+jacobmarc01@users.noreply.github.com>
2024-02-11 15:18:03 -0600

    Enemy manager and win conditions

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,14 +6,23 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private PlayerManager pm;
+    private EnemyManager em;
     public int turnNumber = 0;
     private HexGrid hg;
+    public GameState gameState;
+    public enum GameState
+    {
+        WIN,
+        LOSS,
+        INPROGRESS
 
+    }
 
     public void UpdateTick(){
         turnNumber ++;
         pm.UpdateTick();
         hg.UpdateTick();
+        
     }
 
     public void HandleCombat(Fleet attacker, Fleet defender)
@@ -30,6 +39,32 @@ public class GameManager : MonoBehaviour
 
     void Awake() {
         pm = FindObjectOfType<PlayerManager>();
+        em = FindObjectOfType<EnemyManager>();
         hg = FindObjectOfType<HexGrid>();
+        
+    }
+    private void Update()
+    {
+        CheckWin();
+    }
+
+    void CheckWin()
+    {
+        if (em.enemyControlledPlanets.Count == pm.playerControlledPlanets.Count ||
+            em.enemyControlledPlanets.Count * pm.playerControlledPlanets.Count > 0)
+        {
+
+            gameState = GameState.INPROGRESS;
+        }
+        
+        else if(em.enemyControlledPlanets.Count == 0)
+        {
+            gameState = GameState.WIN;
+        }
+        else if (pm.playerControlledPlanets.Count == 0)
+        {
+            gameState = GameState.LOSS;
+        }
+        
     }
 }
