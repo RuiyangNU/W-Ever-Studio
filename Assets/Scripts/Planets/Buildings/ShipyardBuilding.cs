0466ba62d17909cb54e0f2ad78933ae19ad0e143
James Zieper <jameszieper2025@u.northwestern.edu>
2024-02-20 23:14:24 -0600

    Update Shipyard Building

--- Assets/Scripts/Planets/Buildings/ShipyardBuilding.cs
+++ Assets/Scripts/Planets/Buildings/ShipyardBuilding.cs
@@ -1,15 +1,27 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipyardBuilding : Building
 {
+    [SerializeField]
+    private int turnsLeft;
+
+    private ShipID buildingShip;
+
+    public ShipID BuildingShip { get => buildingShip; }
+
+    public int TurnsLeft { get => turnsLeft; }
+
+    public bool IsActive => turnsLeft != -1;
+
     public ShipyardBuilding(Planet homePlanet)
     {
         this.buildingID = BuildingID.SHIPYARD;
         this.homePlanet = homePlanet;
         this.level = 1;
         this.maxLevel = 2;
+        this.turnsLeft = -1;
 
     }
 
@@ -25,9 +37,13 @@ public class ShipyardBuilding : Building
 
     public override void UpdateTick()
     {
-        return;
+        turnsLeft = Mathf.Max(0, turnsLeft-1);
 
-
+        if (turnsLeft == 0 && !(homePlanet.Occupied))
+        {
+            homePlanet.SpawnPlayerFleet(buildingShip);
+            this.turnsLeft = -1;
+        }
     }
 
     public override Dictionary<Commodity, int> UpgradeCommodityRequirement()
@@ -55,6 +71,58 @@ public class ShipyardBuilding : Building
             default:
                 Debug.LogError("Unknown credit requirement to upgrade a level " + this.level + " ship yard.");
                 return int.MaxValue;
+        }
+    }
+
+    public static int BuildTime(ShipID id)
+    {
+
+         switch (id)
+        {
+            case ShipID.MONO: return 3;
+            case ShipID.FLARE: return 5;
+            case ShipID.SPARK: return 5;
+            case ShipID.PULSE: return 5;
+            case ShipID.EMBER: return 8;
+            case ShipID.VOLT: return 8;
+            case ShipID.BLAST: return 8;
+
+            default:
+                Debug.LogError("Unknown ship ID.");
+                return -1;
+        }
+    }
+
+    public void BuildShip(ShipID id)
+    {
+        if (IsActive)
+        {
+            Debug.LogError("Tried to build a ship at " + homePlanet.name + ", but the shipyard is already building a ship.");
+            return;
+        }
+        if (CanBuild(id) != true)
+        {
+            Debug.LogError("Cannot build" + id.ToString() + " ship with your current shipyard level.");
+            return;
+        }
+        turnsLeft = BuildTime(id);
+        buildingShip = id;
+
+    }
+
+    public bool CanBuild(ShipID id) {
+        switch(id){
+            case ShipID.MONO: return true;
+            case ShipID.FLARE: return true;
+            case ShipID.SPARK: return true;
+            case ShipID.PULSE: return true;
+            case ShipID.EMBER: return this.level >= 2;
+            case ShipID.VOLT: return this.level >= 2;
+            case ShipID.BLAST: return this.level >= 2;
+
+            default:
+                Debug.LogError("Unknown ship ID.");
+                return false;
         }
     }
 }
